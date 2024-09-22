b02694dbb726057e833c164b803643f110935aee
berketonoz <btonoz@sabanciuniv.edu>
2024-09-23 01:27:01 +0300

    react bootstrap navbar implementation init

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import NavBar from "./Components/Navbar/NavBar";
 
 function App() {
   return (
+    <>
+    <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Home />} />
+      <Route path="/blog" element={<Home />} />
     </Routes>
+    </>
   );
 }
 
