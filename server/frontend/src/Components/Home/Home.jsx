2b42e226d1ce18c80b54a7f81869559234012930
berketonoz <btonoz@sabanciuniv.edu>
2024-09-24 15:12:32 +0300

    animation css added for three dots and letters

--- server/frontend/src/Components/Home/Home.jsx
+++ server/frontend/src/Components/Home/Home.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 function Home() {
+  const text = "Welcome to Tony's blog";
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <div className="background-container">
+        <div className="main-container">
+          <div className="content">
+            {text.split("").map((char, index) => (
+              <span key={index} className="animated-letter" style={{ animationDelay: `${index * 0.1}s` }}>
+                {char === " " ? "\u00A0" : char}
+              </span>
+            ))}
+            {/* Dots for the loading effect */}
+            <div className="dots"></div>
+          </div>
+        </div>
+      </div>
+      <div className="test">
+        Test
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
