be5c4ff845bee95fb2993cf344773bfdeb156fba
berketonoz <btonoz@sabanciuniv.edu>
2024-09-24 16:02:32 +0300

    background image added from newly created assets folder

--- server/frontend/src/Components/Home/Home.jsx
+++ server/frontend/src/Components/Home/Home.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Home.css";
+import HomeBg from "../../assets/home-background.jpg"
 
 function Home() {
-  const text = "Welcome to Tony's blog";
+  const text = "Welcome to Tony's Tech Blog";
+  const articlesRef = useRef(null);
+
+  const scrollToArticles = () => {
+    articlesRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <>
-      <div className="background-container">
+      <div className="background-container" style={{backgroundImage: `url(${HomeBg})`}}>
         <div className="main-container">
           <div className="content">
             {text.split("").map((char, index) => (
@@ -14,13 +20,18 @@ function Home() {
                 {char === " " ? "\u00A0" : char}
               </span>
             ))}
-            {/* Dots for the loading effect */}
             <div className="dots"></div>
+          </div>
+          <div className="cta-container">
+            <button className="cta-button" onClick={scrollToArticles}>Explore Articles</button>
           </div>
         </div>
       </div>
-      <div className="test">
-        Test
+      <div ref={articlesRef} className="articles-section">
+        {/* Your articles content here */}
+      </div>
+      <div className="footer">
+        <p>&copy; 2024 Tony's Tech Blog. All rights reserved.</p>
       </div>
     </>
   );
