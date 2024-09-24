4aebb74d9becec472dd1bf4e953e2718537065b5
berketonoz <btonoz@sabanciuniv.edu>
2024-09-25 02:17:50 +0300

    Link added to articles

--- frontend/src/Components/Articles/Articles.jsx
+++ frontend/src/Components/Articles/Articles.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Articles.css"; // Import the external CSS file
+import { Link } from "react-router-dom";
 
 // Static list of beginner-friendly data structure articles
 const articles = [
@@ -26,25 +27,27 @@ const Articles = () => {
       <h1 className="article-header">Articles</h1>
       <ul className="article-list">
         {articles.map((article) => (
-          <li key={article.id} className="article-item">
-            <h2 className="title">{article.title}</h2>
-            <p className="info">
-              <strong>Category:</strong> {article.category}
-            </p>
-            <p className="info">
-              <strong>Programming Languages:</strong> 
-              <span className="languages">
-                {article.programmingLanguages.map((language, index) => (
-                  <span key={index} className="language-badge">
-                    {language}
-                  </span>
-                ))}
-              </span>
-            </p>
-            <p className="info">
-              <strong>Published on:</strong> {article.publishDate}
-            </p>
-          </li>
+          <Link to={`article/${article.id}`}>
+            <li key={article.id} className="article-item">
+              <h2 className="title">{article.title}</h2>
+              <p className="info">
+                <strong>Category:</strong> {article.category}
+              </p>
+              <p className="info">
+                <strong>Programming Languages:</strong>
+                <span className="languages">
+                  {article.programmingLanguages.map((language, index) => (
+                    <span key={index} className="language-badge">
+                      {language}
+                    </span>
+                  ))}
+                </span>
+              </p>
+              <p className="info">
+                <strong>Published on:</strong> {article.publishDate}
+              </p>
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
