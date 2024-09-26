1e04834ba48ccec2ccc9229a13f7bfd88e12cc9e
berketonoz <btonoz@sabanciuniv.edu>
2024-09-27 02:54:04 +0300

    footer component fully separated from home component and integrated globally

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+        <p>&copy; 2024 Tony's Tech Blog. All rights reserved.</p>
+    </div>
   )
 }
 
