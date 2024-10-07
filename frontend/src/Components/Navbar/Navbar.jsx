666b7c52243ff473a8f11012eca41fd969fc6a22
berketonoz <btonoz@sabanciuniv.edu>
2024-10-07 20:03:44 +0300

    responsive hamburger implemented

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import LogoImg from "../../assets/logo.png"
+import LogoImg from "../../assets/logo.png";
 import "./Navbar.css";
 
 function NavbarPanel() {
   const [navbarBg, setNavbarBg] = useState(false);
+  const [expanded, setExpanded] = useState(false); // State to manage Navbar expand/collapse
 
   // Function to handle scroll event
   const handleScroll = () => {
-    if (window.scrollY > 40) {
-      // Scroll threshold, change it as needed
-      setNavbarBg(true);
-    } else {
-      setNavbarBg(false);
-    }
+    setNavbarBg(window.scrollY > 40);
   };
 
   // Add scroll event listener when the component mounts
@@ -22,35 +18,36 @@ function NavbarPanel() {
     window.addEventListener("scroll", handleScroll);
 
     // Clean up event listener when the component unmounts
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <Navbar
-      className={`navbar navbar-expand-lg fixed-top ${
-        navbarBg ? "transparent-bg" : ""
-      }`}
-      variant={navbarBg ? "light" : "dark"} // Switch between light and dark variants
+      expanded={expanded}
+      expand="md" // This ensures the navbar toggles on medium or smaller devices
+      className="fixed-top navbar-dark"
+      onToggle={() => setExpanded(!expanded)}
+      onSelect={() => setExpanded(false)} // Collapse the navbar on selecting an item
+      bg="dark"
     >
       <Container>
-        <Navbar.Brand>
-          <Link to="/">
-            <img src={LogoImg} alt="Logo" className="logo"/>
-          </Link>
+        <Navbar.Brand as={Link} to="/">
+          <img src={LogoImg} alt="Logo" className="logo"/>
         </Navbar.Brand>
-        <Nav>
-          <Link to="/" className="animated-link">
-            Home
-          </Link>
-          <Link to="/about" className="animated-link">
-            About
-          </Link>
-          <Link to="/contact" className="animated-link">
-            Contact
-          </Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" /> {/* This is the hamburger menu button */}
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto"> {/* This aligns the nav items to the right */}
+            <Nav.Link as={Link} to="/" className="animated-link">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about" className="animated-link">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="animated-link">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
