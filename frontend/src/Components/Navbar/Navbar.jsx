b81f44752d59d5b8c6b0366630af8f9b006cf61a
berketonoz <btonoz@sabanciuniv.edu>
2024-10-07 23:34:33 +0300

    navbar mousedown and link click event implemented

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import LogoImg from "../../assets/logo.png";
@@ -7,18 +7,44 @@ import "./Navbar.css";
 function NavbarPanel() {
   const [navbarBg, setNavbarBg] = useState(false);
   const [expanded, setExpanded] = useState(false); // State to manage Navbar expand/collapse
+  const navRef = useRef(null); // Create a reference for the nav collapse
 
   // Function to handle scroll event
   const handleScroll = () => {
     setNavbarBg(window.scrollY > 40);
   };
 
+  // Close the nav collapse when clicking outside
+  const handleClickOutside = (event) => {
+    if ((navRef.current && !navRef.current.contains(event.target)) || window.location.href ) {
+      // Check if the navbar is open
+      const navbarCollapse = document.querySelector(".navbar-collapse");
+      if (navbarCollapse.classList.contains("show")) {
+        // Trigger the collapse if the navbar is open
+        document.querySelector(".navbar-toggler").click();
+      }
+    }
+  };
+
+  // Close the nav collapse when clicking on a link
+  const handleLinkClick = () => {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+      // Trigger the collapse if the navbar is open
+      document.querySelector('.navbar-toggler').click();
+    }
+  };
+
   // Add scroll event listener when the component mounts
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
+    document.addEventListener("mousedown", handleClickOutside);
 
     // Clean up event listener when the component unmounts
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, []);
 
   return (
@@ -29,21 +55,25 @@ function NavbarPanel() {
       onToggle={() => setExpanded(!expanded)}
       onSelect={() => setExpanded(false)} // Collapse the navbar on selecting an item
       bg="dark"
+      ref={navRef}
     >
       <Container>
         <Navbar.Brand as={Link} to="/">
-          <img src={LogoImg} alt="Logo" className="logo"/>
+          <img src={LogoImg} alt="Logo" className="logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" /> {/* This is the hamburger menu button */}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />{" "}
+        {/* This is the hamburger menu button */}
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto"> {/* This aligns the nav items to the right */}
-            <Nav.Link as={Link} to="/" className="animated-link">
+          <Nav className="ml-auto">
+            {" "}
+            {/* This aligns the nav items to the right */}
+            <Nav.Link as={Link} to="/" className="animated-link" onClick={handleLinkClick}>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/about" className="animated-link">
+            <Nav.Link as={Link} to="/about" className="animated-link" onClick={handleLinkClick}>
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="/contact" className="animated-link">
+            <Nav.Link as={Link} to="/contact" className="animated-link" onClick={handleLinkClick}>
               Contact
             </Nav.Link>
           </Nav>
