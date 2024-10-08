3ec1f3169660ab3d84e462ff207a66f91335da8a
berketonoz <btonoz@sabanciuniv.edu>
2024-10-08 04:16:50 +0300

    footer bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,17 @@ import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import About from "./Components/Aboutme/AboutMe";
 import Footer from "./Components/Footer/Footer";
-import Tutorial from "./Components/Articles/Tutorial";
+import Tutorial from "./Components/Tutorials/Tutorial";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/tutorial/:id" element={<Tutorial />} />
-    </Routes>
-    <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/tutorial/:id" element={<Tutorial />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useRef } from "react";
 import "./Home.css";
 import HomeBg from "../../assets/home-background.jpg";
-import Tutorials from "../Articles/Tutorials";
+import Tutorials from "../Tutorials/Tutorials";
+import Footer from "../Footer/Footer";
 
 function Home() {
   const text = "Welcome to Tony's Tech Blog";
@@ -13,24 +14,34 @@ function Home() {
 
   return (
     <>
-      <div className="background-container" style={{ backgroundImage: `url(${HomeBg})` }}>
+      <div
+        className="background-container"
+        style={{ backgroundImage: `url(${HomeBg})` }}
+      >
         <div className="main-container">
-          <div className="content" style={{flexDirection: "column"}}>
+          <div className="content" style={{ flexDirection: "column" }}>
             <div className="welcome-text">
               {text.split("").map((char, index) => (
-                <span key={index} className="animated-letter" style={{ animationDelay: `${index * 0.1}s` }}>
+                <span
+                  key={index}
+                  className="animated-letter"
+                  style={{ animationDelay: `${index * 0.1}s` }}
+                >
                   {char === " " ? "\u00A0" : char}
                 </span>
               ))}
             </div>
             <div className="dots"></div>
-            <button className="cta-button" onClick={scrollToTutorials}>Explore Tutorials</button>
+            <button className="cta-button" onClick={scrollToTutorials}>
+              Explore Tutorials
+            </button>
           </div>
         </div>
       </div>
       <div ref={tutorialsRef} className="tutorials-section">
         <Tutorials />
       </div>
+      <Footer />
     </>
   );
 }
