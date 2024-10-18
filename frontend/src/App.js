2f9bc79a6077c178730aca9d137003f660e7596a
berketonoz <btonoz@sabanciuniv.edu>
2024-10-19 02:23:33 +0300

    darkmode enhanced and projects component and route created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,27 @@ import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import About from "./Components/Aboutme/AboutMe";
 import Tutorial from "./Components/Tutorials/Tutorial";
+import Projects from "./Components/Projects/Projects";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    document.body.style.transition = "background-color 0.4s ease-in-out";
-    document.body.style.backgroundColor = darkMode ? "#212529": "#f9f9f9";
-  },[darkMode])
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
 
   return (
     <>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <Routes>
         <Route path="/" element={<Home darkMode={darkMode} />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/tutorial/:id" element={<Tutorial darkMode={darkMode}/>} />
+        <Route path="/about" element={<About darkMode={darkMode} />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/tutorial/:id" element={<Tutorial darkMode={darkMode} />} />
       </Routes>
     </>
   );
