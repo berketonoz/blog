85b787c0edfb95e3ed7eb22e9903c396a15a715b
berketonoz <btonoz@sabanciuniv.edu>
2024-10-21 03:23:53 +0300

    projects.jsx init

--- frontend/src/Components/Projects/Projects.jsx
+++ frontend/src/Components/Projects/Projects.jsx
@@ -3,7 +3,9 @@ import './Projects.css'
 
 function Projects() {
   return (
-    <div className='projects-container'>Projects</div>
+    <div className='projects-container'>
+      
+    </div>
   )
 }
 
